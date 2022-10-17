fab7693953a1de020789823ed805f3dfd5effd8a
pkchitya <pkchitya@iu.edu>
2022-10-17 16:22:43 -0400

    client_changes

--- client.py
+++ client.py
@@ -35,14 +35,14 @@ def getDataPoint(quote):
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = (bid_price+ask_price)/2
+    price = round((bid_price+ask_price)/2,2)
     return stock, bid_price, ask_price, price
 
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
-    return price_a/price_b
+    return round(price_a/price_b,4)
 
 
 # Main
